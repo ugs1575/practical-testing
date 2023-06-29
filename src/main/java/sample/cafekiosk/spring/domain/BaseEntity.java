06af26b4e01da90dc1105e5985444a987a9835e4
우경서 <wgs1575@jiran.com>
2023-06-29 12:34:22 +0900

    프로젝트 세팅, 간단한 단위테스트 추가

---
+++ src/main/java/sample/cafekiosk/spring/domain/BaseEntity.java
@@ -0,0 +1,25 @@
+package sample.cafekiosk.spring.domain;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+import org.springframework.context.event.EventListener;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+	@CreatedDate
+	private LocalDateTime createdDateTime;
+
+	@LastModifiedBy
+	private LocalDateTime modifiedDateTime;
+}
